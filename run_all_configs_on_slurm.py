3a01b83328901dcb6dd63dab7cd5505900f0ade0
sjtufs <sjtufs@gmail.com>
2020-05-20 16:02:53 -0400

    udpate slurm

--- run_all_configs_on_slurm.py
+++ run_all_configs_on_slurm.py
@@ -2,17 +2,17 @@ import glob
 import subprocess
 
 header = '''#!/bin/bash
-#SBATCH --qos=gpu-medium
 #SBATCH --partition=gpu
+#SBATCH --qos=gpu-medium
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=snli
-#SBATCH --time=0-08:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --chdir=/fs/clip-scratch/shifeng/influenceless
-#SBATCH --output=/fs/clip-scratch/shifeng/influenceless
 #SBATCH --exclude=materialgpu00
 
-'''
+nvidia-smi
 
+'''
 
 for i, filename in enumerate(glob.iglob('configs/SNLI/**/*.json', recursive=True)):
     if 'base' in filename:
